cf45f960aca7d17848181f48b6999805c857d957
hamely <sarmiento101147@gmail.com>
2019-03-18 17:52:53 -0500

    A

--- resources/views/frondend/contacto.blade.php
+++ resources/views/frondend/contacto.blade.php
@@ -170,21 +170,21 @@
                             <div class="col-md-6">
                                 <div class="c-contact">
                                     <div class="c-content-title-1">
-                                        <h3 class="c-font-uppercase c-font-bold">Keep in touch</h3>
+                                        <h3 class="c-font-uppercase c-font-bold">Contáctenos</h3>
                                         <div class="c-line-left"></div>
                                         <p class="c-font-lowercase">Our helpline is always open to receive any inquiry or feedback. Please feel free to drop us an email from the form below and we will get back to you as soon as we can.</p>
                                     </div>
                                     <form action="#">
                                         <div class="form-group">
-                                            <input type="text" placeholder="Your Name" class="form-control c-square c-theme input-lg"> </div>
+                                            <input type="text" placeholder="Nombre Completo" class="form-control c-square c-theme input-lg"> </div>
                                         <div class="form-group">
-                                            <input type="text" placeholder="Your Email" class="form-control c-square c-theme input-lg"> </div>
+                                            <input type="text" placeholder="Correo electrónico" class="form-control c-square c-theme input-lg"> </div>
                                         <div class="form-group">
-                                            <input type="text" placeholder="Contact Phone" class="form-control c-square c-theme input-lg"> </div>
+                                            <input type="text" placeholder="Número telefónico" class="form-control c-square c-theme input-lg"> </div>
                                         <div class="form-group">
-                                            <textarea rows="8" name="message" placeholder="Write comment here ..." class="form-control c-theme c-square input-lg"></textarea>
+                                            <textarea rows="8" name="message" placeholder="Dejenos su mensaje o consulta aquí" class="form-control c-theme c-square input-lg"></textarea>
                                         </div>
-                                        <button type="submit" class="btn c-theme-btn c-btn-uppercase btn-lg c-btn-bold c-btn-square">Submit</button>
+                                        <button type="submit" class="btn c-theme-btn c-btn-uppercase btn-lg c-btn-bold c-btn-square">Enviar</button>
                                     </form>
                                 </div>
                             </div>
